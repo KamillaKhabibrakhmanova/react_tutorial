876a5d30d418a351ad2f25bc1b71c2a2c0681c37
Kamilla <venessiel@gmail.com>
2017-04-07 15:06:40 -0400

    change searchbar to be a class-based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+// same as const Component = React.Component
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+// class-based component needed when you need internal record keeping
+// searchBar has access to all of React.Component's functionality
+class SearchBar extends Component {
+	//every class-based React component must have a render method
+	render() {
+		return <input />
+	}
+}
 
 export default SearchBar;
