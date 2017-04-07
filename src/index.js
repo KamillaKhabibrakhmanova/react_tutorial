738849084b537ba5e46b2cb2eae4f1087069da49
Kamilla <venessiel@gmail.com>
2017-04-07 09:28:21 -0400

    first page render complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-// Create a new component. Should produce some HTML
+//transpiler will grab React
+//core react library knows how to work with/render reach components
+import React from 'react';
+//library to render react components into the DOM
+import ReactDOM from 'react-dom';
 
-const App = function(){
+//a factory that creates instances
+//a class, not an instance of the components
+const App = () => {
 	return <div>Hi!</div>;
 }
 
 //Take this component's generated HTML and put it in the DOM
+//Render component = place component's HTML in the DOM
+//second argument = target DOM node on the page (where to insert it)
+ReactDOM.render(<App />, document.querySelector('.container'));
