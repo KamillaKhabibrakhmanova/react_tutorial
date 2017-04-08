e72612ef96ee6d1f712dbe85959598982a6980b5
KamillaKhabibrakhmanova <venessiel@gmail.com>
2017-04-08 12:26:26 -0400

    youtube video search

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 //transpiler will grab React
 //core react library knows how to work with/render reach components
-import React from 'react';
+import React, { Component } from 'react';
 //library to render react components into the DOM
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBi5pv6ffX4arjV1Ti4qtfC1EOR39yAd8A';
 
+
 //a factory that creates instances
 //a class, not an instance of the components
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: []};
+
+
+		YTSearch({key: API_KEY, term: 'surfboards' }, (videos) => {
+			this.setState({ videos });
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	};
 }
 
 //Take this component's generated HTML and put it in the DOM
